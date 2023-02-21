b022b6344626a68688d04e571d8cde526d711904
Ivan K <ikolodeev002@gmail.com>
2023-02-21 15:50:17 +0200

    Homework

---
+++ Excersise_16/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// void PrintArray(int[] col)
+// {
+//     for (int i = 0; i < col.Length; i++)
+//     Console.Write(col[i] + "  ");
+// }
+
+void NumberToArray(int number, int[] array)
+{
+    while (number > 0)
+    {
+        int digit = number % 10;
+        array[number.ToString().Length - 1] = digit;
+        number /= 10;       
+    }
+}
+
+Console.WriteLine("Enter number");
+int a = int.Parse(Console.ReadLine()!);
+int length = a.ToString().Length;
+
+if (length != 5)  Console.WriteLine("Not 5-digit number");
+else
+{
+    // Преобразование числа в массив
+    int[] array = new int[length];
+    NumberToArray(a, array);
+    // PrintArray(array);
+
+    // Второй массив - реверс первого
+    int[] array2 = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+     array2[i] = array[length - 1 - i];
+    }
+    // Console.WriteLine();
+    // PrintArray(array2);
+
+    // сравнение двух массивов
+    Boolean isArrayEqual = true;
+    for (int i = 0; i < length; i++)
+    if (array[i] != array2[i])
+    isArrayEqual = false;
+
+    // вывод результатов сравнения
+    if (isArrayEqual == false)
+    Console.WriteLine("Noooooo");
+    else
+    // Console.WriteLine();
+    Console.WriteLine("Polindrom");
+}
---
+++ Excersise_21/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Please enter A coordinates:");
+Console.Write("Ax = ");
+int Ax = int.Parse(Console.ReadLine()!);
+Console.Write("Ay = ");
+int Ay = int.Parse(Console.ReadLine()!);
+Console.Write("Az = ");
+int Az = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Please enter B coordinates:");
+Console.Write("Bx = ");
+int Bx = int.Parse(Console.ReadLine()!);
+Console.Write("By = ");
+int By = int.Parse(Console.ReadLine()!);
+Console.Write("Bz = ");
+int Bz = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("A(" + Ax + "," + Ay + "," + Az + ")");
+Console.WriteLine("B(" + Bx + "," + By + "," + Bz + ")");
+
+double dist = Math.Round(Math.Sqrt(Math.Pow((Bx-Ax),2) + Math.Pow((By-Ay),2) + Math.Pow((Bz-Az),2)),2);
+Console.WriteLine("Distanation = " + dist);
